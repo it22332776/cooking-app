e0e0bb60cb78414e12d79d09db7c892b5afa76f6
Jayani1011 <127919285+it22332776@users.noreply.github.com>
2025-04-27 17:02:19 +0530

    create StatusRepository.java backend file

--- backend/src/main/java/com/example/backend/repository/StatusRepository.java
+++ backend/src/main/java/com/example/backend/repository/StatusRepository.java
@@ -1,11 +1,12 @@
 package com.example.backend.repository;
 
-import com.example.backend.model.Status;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
-import java.util.List;
+import com.example.backend.model.Status;
 
 @Repository
 public interface StatusRepository extends MongoRepository<Status, String> {
